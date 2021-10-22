28702285858a529ce4e30ba3f68960121a9777d5
Jim Ryan <jimmih15@gmail.com>
2021-10-22 19:30:16 +0100

    Chapter 8 pt 1

---
+++ Chapter_8/game.lisp
@@ -0,0 +1,15 @@
+(load "graph-util")
+
+(defparameter *congestion-city-nodes* nil)
+
+(defparameter *congestion-city-edges* nil)
+
+(defparameter *visited-nodes* nil)
+
+(defparameter *node-num* 30)
+
+(defparameter *edge-num* 45)
+
+(defparameter *worm-num* 3)
+
+(defparameter *cop-odds* 15)
